19a0e16e7c10972ecf93eb4380b440163fa36b4d
Rami Al-Rfou <rmyeid@gmail.com>
2011-12-04 00:15:27 -0500

    add settings module to reduce the number of modification needed to source code, has to split the featureset computation from the algorithm

---
+++ Project/settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""settings.py: A module that contain some settings."""
+
+ACCEPTED_LANGUAGES = { 'User_en-us',
+                        'User_de-N',
+                        'User_nl-N',
+                        'User_ru-N',
+                        'User_es-N',
+                        'User_fr-N'
+                      }
+
+LEN_1SENT = 1
+NUM_SENTS = 1
